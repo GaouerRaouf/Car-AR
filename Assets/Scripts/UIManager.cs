11ad6c80df2935046b684eae3fd082bcaf365c3e
Abderraouf Gaouer <gaouerraouf02@gmail.com>
2025-03-20 03:09:24 +0100

    updated settings and added UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject currentPage;
+
+    public void ChangePage(GameObject page)
+    {
+        currentPage.SetActive(false);
+        currentPage = page;
+        currentPage.SetActive(true);
+    }
+
+    public void SetCurrentPage(GameObject page)
+    {
+        currentPage = page;
+        currentPage.SetActive(true);
+    }
+
+    public void OpenTabAdd(GameObject tab)
+    {
+        tab.SetActive(true);
+    }
+
+    public void CloseTabAdd(GameObject tab)
+    {
+        tab.SetActive(false);
+    }
+
+
+}
